b61ab59117c991854e2c24bf45e93842a3db63d4
wpeguero <wpeguerorosario@outlook.com>
2024-01-23 17:08:15 -0500

    NEW: Test method within Trainer Class
    Created a new method called test whose sole purpose is testing how
    accurately the machine learning model is able to classify images.

--- src/trainers.py
+++ src/trainers.py
@@ -3,6 +3,8 @@ import torch
 from torch import nn, optim
 from torch.utils import data
 
+version = 1
+
 class TrainModel:
     """Class for training pytorch machine learning models.
 
@@ -85,5 +87,44 @@ class TrainModel:
                 total += labels.size(0)
                 correct += (ipredicted == lindices).sum().item()
                 running_loss += loss.item()
-                if i+1 == steps_per_epoch:
-                    print(f'[{epoch + 1:3d}/{epochs}, {i + 1:5d}] loss: {running_loss / steps_per_epoch:.3f}, accuracy: {round(100 * correct / total, 2)}')
+            print(f'[{epoch + 1:3d}/{epochs}] loss: {running_loss / steps_per_epoch:.3f}, accuracy: {round(100 * correct / total, 2)}')
+
+    def test(self, testloader:data.DataLoader, classes:tuple, gpu=False):
+        """Test the model's ability to classify on a never before seen dataset.
+
+        Parameters
+        ----------
+        testloader : pytorch DataLoader
+            Dataset that was never seen by the model.
+        classes : tuple
+            An immutable list of classes from the dataset.
+        gpu : bool
+            Determines whether to send the model and data to the gpu
+            or the cpu.
+        """
+        if  gpu == True:
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        else:
+            device = torch.device("cpu")
+        correct_pred = {classname: 0 for classname in classes}
+        total_pred = {classname: 0 for classname in classes}
+        results = list("Accuracy Results on test set for Machine Learning Model {}\n".format(self.model.__class__.__name__))
+
+        with torch.no_grad():
+            for images, labels in testloader:
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = self.model(images)
+                _, predictions = torch.max(outputs, 1)
+                _, lindices = torch.max(labels, 1)
+                for label, pred in zip(lindices, predictions):
+                    if label == pred:
+                        correct_pred[classes[label]] += 1
+                    total_pred[classes[label]] += 1
+        for classname, correct_count in correct_pred.items():
+            accuracy = 100 * float(correct_count) / total_pred[classname]
+            results.append(f'Accuracy for class: {classname:5s} is {accuracy:.1f}%\n')
+            print(f'Accuracy for class: {classname:5s} is {accuracy:.1f}%')
+        with open('data/{}_results_version_{}.txt'.format(self.model.__class__.__name__, version), 'w') as fp:
+            fp.writelines(results)
+            fp.close()
