94457c924c38127392f05d07d2fb4b89b7ff4c6b
wpeguero <wpeguerorosario@outlook.com>
2024-04-10 08:46:20 -0400

    CHANGE: Started Building the Hangman Env
    Started the creation of the Hangman environment using pytorchrl.
    Currently have the class initialized and am reading an example using a
    custom pendulum environment.

--- src/rl/environments.py
+++ src/rl/environments.py
@@ -4,4 +4,40 @@ Contains all of the custom environments for building machine learning
 models.
 """
 
+import torchrl
+import tensordict
 
+def _main():
+    """Test classes."""
+    pass
+
+
+class HangmanEnvironment(torchrl.envs.EnvBase):
+    """Pytorch Implementation of the Hangman Environment.
+
+    This is an implementation based on the TensorFlow Hangman
+    Environment made for the job application.
+
+    Arguments
+    ---------
+    words : list
+        Contains a list of words for playing hangman.
+    """
+
+    def __init___(self, words:list):
+        """Init the class."""
+        super().__init__(device=device, batch_size=[])
+
+    def _step(self, td:tensordict.TensorDictBase):
+        """Compute the Next Step for the actor.
+
+        Parameters
+        ----------
+        td: TensorDictBase
+            Contains the data required for calculating the next step.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    _main()
