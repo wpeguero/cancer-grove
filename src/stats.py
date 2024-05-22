5efc202371a2a8a305c5a018b9f41c28098569c6
wpeguero <wpeguerorosario@outlook.com>
2024-05-22 19:00:55 -0400

    NEW: Function to calculate image based t-test
    Created a function to compare images belonging to two different classes
    based on mean pixel magnitude.

--- src/stats.py
+++ src/stats.py
@@ -1,6 +1,9 @@
 """Useful Functions for Statistical Analysis."""
 
 import torch
+from torch.utils import data
+import polars as pl
+import scipy
 
 def calculate_confusion_matrix(fin_predictions:pl.DataFrame):
     """Calculate the confusion matrix using pandas.
@@ -43,3 +46,37 @@ def calculate_confusion_matrix(fin_predictions:pl.DataFrame):
     metrics['Recall'] = tp / (tp + fn) # Ability of model to correctly identify positives
     metrics['F1 Score'] = (2 * metrics['Precision'] * metrics['Recall']) / (metrics['Precision'] + metrics['Recall'])
     return ct, metrics
+
+def calculate_image_t_test(dataset:data.Dataset, labels:dict, ci:int=0.95): #two sample t-test of unequal variance; Requires Correction of Degrees of Freedom
+    """Calculate the t-test for comparing two samples of data with unequal variance."""
+    assert ci > 0.0 and ci < 1.0, "the parameter `ci` must fall between 0 and 1.\nCurrent value: {}".format(ci)
+    means = dict()
+    standard_deviations = dict()
+    variances = dict()
+    counts = dict()
+    for k,v in labels.items():
+        count = 0
+        means.update({k:torch.mean(torch.Tensor([torch.mean(X) for X,y in dataset if int(y.argmax().item()) == v]))})
+        standard_deviations.update({k:torch.std(torch.Tensor([torch.mean(X) for X,y in dataset if int(y.argmax().item()) == v]))})
+        variances.update({k:torch.var(torch.Tensor([torch.mean(X) for X,y in dataset if int(y.argmax().item()) == v]))})
+        for _,y in dataset:
+            if y.argmax().item() == v:
+                count+=1
+        counts.update({k:count})
+    base_stats = {'means':means, 'stds':standard_deviations, 'variances':variances, 'counts':counts}
+    m1 = means['cat']
+    m2 = means['loaf']
+    std1 = standard_deviations['cat']
+    std2 = standard_deviations['loaf']
+    var1 = variances['cat']
+    var2 = variances['loaf']
+    n1 = counts['cat']
+    n2 = counts['loaf']
+    t_value = (m1-m2)/ torch.sqrt((var1/n1) + (var2/n2))
+    df_numerator = torch.square((torch.square(var1)/n1) + (torch.square(var2)/n2))
+    df_denominator = (torch.square(torch.square(var1)) / ((n1**2)*(n1-1))) + (torch.square(torch.square(var2)) / ((n2**2)*(n2-1)))
+    df = df_numerator / df_denominator
+    #t = scipy.stats.t.ppf(ci, df)
+    print('t value: ', t_value)
+    print('degrees of freedom: {}'.format(df))
+    return base_stats
