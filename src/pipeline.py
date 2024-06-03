d642063cafc84863374c1695f732ea6b5bfed69e
wpeguero <wpeguerorosario@outlook.com>
2024-06-03 14:02:10 -0400

    CHANGE: Testing stats
    Currently using the pipeline module for testing other modules.

--- src/pipeline.py
+++ src/pipeline.py
@@ -10,8 +10,9 @@ Archive.
 import os
 import pathlib
 import json
-from collections import defaultdict
+from typing import Optional
 import re
+import time
 
 import numpy as np
 import plotly.express as px
@@ -27,6 +28,8 @@ from torchvision import datasets, transforms
 from models import CustomCNN, AlexNet, InceptionStem, InceptionA, InceptionB, InceptionC, ReductionA, ReductionB, InceptionV4, TutorialNet
 from datasets import DICOMSet
 from trainers import Trainer, VERSION
+from utils import create_target_transform, STANDARD_IMAGE_TRANSFORMS
+from stats import calculate_image_t_test
 
 img_size = (512, 512)
 torch.manual_seed(42)
@@ -38,13 +41,15 @@ def _main():
     dir_train = "data/images/"
     cat_trans = create_target_transform(2)
     img_set = datasets.ImageFolder(root=dir_train, transform=STANDARD_IMAGE_TRANSFORMS, target_transform=cat_trans)
+    print(img_set.targets)
+    print(img_set.class_to_idx)
+    print(img_set[0][1].max().item())
+    print(calculate_image_t_test(img_set, img_set.class_to_idx, 0.99))
+    exit()
     train_size = int(0.7*len(img_set))
     val_size = len(img_set) - train_size
     train_set, val_set = data.random_split(img_set, [train_size, val_size])
-    dets = list()
-    for i, (image, label) in enumerate(train_set):
-        mean_pixel_intensity = torch.mean(image)
-        dets.append(mean_pixel_intensity)
+    dets = [torch.mean(image) for image, label in train_set]
     img_mean = torch.mean(torch.Tensor(dets))
     img_std = torch.std(torch.Tensor(dets))
     print("mean: {}\nStandard Deviation: {}".format(img_mean, img_std))
@@ -65,6 +70,7 @@ def _main():
     trained_model1 = trainer1.get_model()
     trainer1.test(trained_model1, val_loader, classes=('cat', 'loaf'),gpu=True)
     torch.save(trained_model1.state_dict(), "models/inceptionv4_catloaf.pt")
+
 
 if __name__ == "__main__":
     _main()
