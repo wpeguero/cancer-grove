1c6863aa6c4e86e2baea338c81b3d08631c8d49d
wpeguero <wpeguerorosario@outlook.com>
2024-06-04 14:18:38 -0400

    CHANGE: Started Extracting the paths and unique ID
    Created an algorithm that would extract the paths from the root dir as
    well as some data to associate the paths to the dicom files to some
    metadata located within excel files.

--- src/pipeline.py
+++ src/pipeline.py
@@ -28,7 +28,7 @@ from torchvision import datasets, transforms
 from models import CustomCNN, AlexNet, InceptionStem, InceptionA, InceptionB, InceptionC, ReductionA, ReductionB, InceptionV4, TutorialNet
 from datasets import DICOMSet
 from trainers import Trainer, VERSION
-from utils import create_target_transform, STANDARD_IMAGE_TRANSFORMS
+import utils
 from stats import calculate_image_t_test
 
 img_size = (512, 512)
@@ -38,38 +38,26 @@ torch.cuda.manual_seed(42)
 
 def _main():
     """Test the new functions."""
-    dir_train = "data/images/"
-    cat_trans = create_target_transform(2)
-    img_set = datasets.ImageFolder(root=dir_train, transform=STANDARD_IMAGE_TRANSFORMS, target_transform=cat_trans)
-    print(img_set.targets)
-    print(img_set.class_to_idx)
-    print(img_set[0][1].max().item())
-    print(calculate_image_t_test(img_set, img_set.class_to_idx, 0.99))
-    exit()
-    train_size = int(0.7*len(img_set))
-    val_size = len(img_set) - train_size
-    train_set, val_set = data.random_split(img_set, [train_size, val_size])
-    dets = [torch.mean(image) for image, label in train_set]
-    img_mean = torch.mean(torch.Tensor(dets))
-    img_std = torch.std(torch.Tensor(dets))
-    print("mean: {}\nStandard Deviation: {}".format(img_mean, img_std))
-    exit()
-    train_loader = data.DataLoader(train_set, batch_size=64, shuffle=True, num_workers=4)
-    val_loader = data.DataLoader(val_set, batch_size=64, shuffle=True, num_workers=4)
-    #Loading the models
-    model1 = InceptionV4(2, 1)
-    #Loading optimizers
-    opt1 = optim.Adam(model1.parameters(), lr=0.003)
-    #Loading the Losses
-    loss1 = nn.BCELoss()
-    #Loading the Trainers
-    trainer1 = Trainer(model1, opt1, loss1)
-    # Training and saving models
-    trainer1.train(train_loader, 160, gpu=True)
-
-    trained_model1 = trainer1.get_model()
-    trainer1.test(trained_model1, val_loader, classes=('cat', 'loaf'),gpu=True)
-    torch.save(trained_model1.state_dict(), "models/inceptionv4_catloaf.pt")
+    dir = 'data/CBIS-DDSM/images/'
+    fpaths = utils.get_file_paths(dir, 'data/CBIS-DDSM/paths.csv')
+    df__paths = pl.read_csv('data/CBIS-DDSM/paths.csv')
+    pathset = list()
+    for row in df__paths.iter_rows(named=True):
+        fpath = row['paths']
+        fpath = fpath[:-1]
+        components = fpath.split('/')
+        unique_id = components[3]
+        if 'ROI mask' in fpath:
+            label = 'mask'
+        elif 'cropped images' in fpath:
+            label = 'crop'
+        elif 'full mammogram images' in fpath:
+            label = 'full'
+        else:
+            label = ''
+        pathset.append({'paths':fpath, 'UID':unique_id, 'img type':label})
+    df__nupaths = pl.DataFrame(pathset)
+    df__nupaths.write_csv('data/CBIS-DDSM/nupaths.csv')
 
 
 if __name__ == "__main__":
